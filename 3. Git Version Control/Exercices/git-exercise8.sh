df5a68ec7f86e257e2b652a72912c3546aa70168
DevCTx <contact.devct@gmail.com>
2026-01-07 18:56:06 +0100

    Add ex 7 and 8

---
+++ 3. Git Version Control/Exercices/git-exercise8.sh
@@ -0,0 +1,11 @@
+# modif Bruno's role in index.html
+#        <li>Bruno - DevOps operations</li>
+# instead of 
+#        <li>Bruno - IT operations</li>
+
+git add .
+git commit -m "modif Bruno's role in index.html"
+
+# then to reset the last commit unpushed
+git reset --hard HEAD~1
+
