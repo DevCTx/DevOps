af20238c3399246f7702cdbdb149feec023cf810
DevCTx <contact.devct@gmail.com>
2026-03-10 09:23:03 +0100

    modif Jenkinsfile or script.groovy

--- PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/script.groovy
+++ PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/script.groovy
@@ -1,25 +1,33 @@
 def buildJar() {
-	echo "Building JAR file ..."
+	echo "buildjar()"
 
     dir("./PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/") {
         sh "pwd"
         sh "ls -la"
+
+    	echo "Building JAR file ..."
     	sh "mvn clean package"
     }
 }
 
 def buildAndPushImage() {
-	echo "Building docker image ..."
+	echo "buildAndPushImage()"
 
     dir("./PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/") {
+        sh "pwd"
+        sh "ls -la"
+
+    	echo "withCredentials"
 	    withCredentials( [
 		    usernamePassword( credentialsId: 'dockerhub-credentials',
                             usernameVariable: 'DOCKER_USERNAME',
 						    passwordVariable: 'DOCKER_PASSWORD' )
 	    ]) {
-		    echo "Pushing docker image to Docker Hub ..."
+        	echo "docker build image ..."
 		    sh "docker build -t devct/demo-java-app:${VERSION} ."
+        	echo "login docker ..."
 		    sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+        	echo "docker push image ..."
 		    sh "docker push devct/demo-java-app:${VERSION}"
 	    }
     }
