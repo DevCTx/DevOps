9c4b9fc88f2d5a54e9386fbe6c9c57ff4593223d
DevCTx <contact.devct@gmail.com>
2026-03-10 09:11:44 +0100

    modif Jenkinsfile or script.groovy

--- PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/script.groovy
+++ PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/script.groovy
@@ -1,22 +1,26 @@
 def buildJar() {
 	echo "Building JAR file ..."
-    cd "./PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/"
-	sh "mvn clean package"
+
+    dir("./PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/") {
+    	sh "mvn clean package"
+    }
 }
 
 def buildAndPushImage() {
 	echo "Building docker image ..."
-	cd "./PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/"
-	withCredentials( [
-		usernamePassword( credentials: 'dockerhub-credentials',
-                        usernameVariable: 'DOCKER_USERNAME',
-						passwordVariable: 'DOCKER_PASSWORD' )
-	]) {
-		echo "Pushing docker image to Docker Hub ..."
-		sh "docker build -t devct/demo-java-app:${VERSION} ."
-		sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
-		sh "docker push devct/demo-java-app:${VERSION}"
-	}
+
+    dir("./PART3 - Core/8. Jenkins CI-CD/java-dockerhub-pipeline/") {
+	    withCredentials( [
+		    usernamePassword( credentials: 'dockerhub-credentials',
+                            usernameVariable: 'DOCKER_USERNAME',
+						    passwordVariable: 'DOCKER_PASSWORD' )
+	    ]) {
+		    echo "Pushing docker image to Docker Hub ..."
+		    sh "docker build -t devct/demo-java-app:${VERSION} ."
+		    sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+		    sh "docker push devct/demo-java-app:${VERSION}"
+	    }
+    }
 }
 
 return this
