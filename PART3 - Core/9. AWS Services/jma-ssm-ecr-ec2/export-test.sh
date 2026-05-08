f34605da35cab267871ccd8811b7eb41faa71471
DevCTx <contact.devct@gmail.com>
2026-05-08 17:32:26 +0200

    Add AWS Exercises

--- PART3 - Core/9. AWS Services/jma-ssm-ecr-ec2/export-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-export IMAGE_NAME=devct/demo-java-app
-export VERSION=3.0
-export BUILD_NUMBER=00
-export IMAGE_TAG=3.0.00
-export HOST_PORT=3080
-export CONTAINER_PORT=3080
-export CONTAINER_NAME=java-app
